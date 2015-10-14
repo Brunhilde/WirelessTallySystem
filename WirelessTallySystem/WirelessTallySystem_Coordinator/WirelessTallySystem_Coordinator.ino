ad365dad359f132e6b43dd0d588bde1e0387336a
Tobias Dieterich <t.dieterich@outlook.com>
2015-10-14 12:00:06 +0200

    first commit

---
+++ WirelessTallySystem/WirelessTallySystem_Coordinator/WirelessTallySystem_Coordinator.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		WirelessTallySystem_Coordinator.ino
+ Created:	14.10.2015 11:58:21
+ Author:	Tobias Dieterich
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
